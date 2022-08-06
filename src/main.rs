a020700ca9119f8859e00e13580a753fbf8ad4c1
Alexander Zaytsev <yup.disconnected.yup@gmail.com>
2022-08-06 23:30:47 +0300

    Playback sine wave from ogg file

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,31 @@
 use text_io::read;
 use morse::encode;
+use std::time::Duration;
+use std::fs::File;
+use std::io::BufReader;
+use rodio::{Decoder, OutputStream, Source};
 
-fn main() {
-
-// Get string from user 
-let line: String = read!("{}\n");
-println!("Your input: {}", line);
-
-// Print String by words
-for word in line.split_whitespace()
+fn main() 
 {
-    let morse_code: String = encode::encode(word).unwrap();
-    println!("{}", morse_code);
+    // Get string from user 
+    let line: String = read!("{}\n");
+    println!("Your input: {}", line);
+
+    // Print String by words
+    for word in line.split_whitespace()
+    {
+        let morse_code: String = encode::encode(word).unwrap();
+        println!("{}", morse_code);
+    }
+
+    let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+   
+    let file = BufReader::new(File::open("sound/sine.ogg").unwrap());
+
+    let source = Decoder::new(file).unwrap();
+
+    stream_handle.play_raw(source.convert_samples());
+
+    std::thread::sleep(std::time::Duration::from_secs(5));
 }
 
-}
