2884101fe7cb31c6951884bed83e9657a673fe4d
Alexander Zaytsev <yup.disconnected.yup@gmail.com>
2022-08-06 23:54:19 +0300

    Add oscillator source provided by Jan Wilczek

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ use std::fs::File;
 use std::io::BufReader;
 use rodio::{Decoder, OutputStream, Source, source::SineWave, cpal::SampleRate};
 
+mod oscillator;
+
 fn main() 
 {
     // Get string from user 
@@ -21,10 +23,29 @@ fn main()
 
     }
 
+    //oscillator::run();
+
+    let wave_table_size = 64;
+    let mut wave_table: Vec<f32> = Vec::with_capacity(wave_table_size);
+
+    for n in 0..wave_table_size {
+        wave_table.push((2.0 * std::f32::consts::PI * n as f32 / wave_table_size as f32).sin());
+    }
+    
+    let mut osc = oscillator::WavetableOscillator::new(48000, wave_table);
+
+    osc.set_frequency(880.0);
+    
     let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    let mut sine = SineWave::new(880.0);  
-    let _result = stream_handle.play_raw(sine.convert_samples());
+    
+    let _result = stream_handle.play_raw(osc.convert_samples());
 
     std::thread::sleep(std::time::Duration::from_secs(1));
+
+    //let (_stream, stream_handle) = OutputStream::try_default().unwrap();
+    //let mut sine = SineWave::new(880.0);  
+    //let _result = stream_handle.play_raw(sine.convert_samples());
+
+    //std::thread::sleep(std::time::Duration::from_secs(1));
 }
 
