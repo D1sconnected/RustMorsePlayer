a6a587829a1f63e04c5b6a468889b3fb24b7caca
Alexander Zaytsev <yup.disconnected.yup@gmail.com>
2022-08-07 00:05:39 +0300

    Add parse of dots and dashes

--- src/main.rs
+++ src/main.rs
@@ -13,20 +13,31 @@ fn main()
     let line: String = read!("{}\n");
     println!("Your input: {}", line);
 
-    // Print String by words
+    // Prepare vector for playback
+    let wave_table_size = 64;
+    let mut wave_table: Vec<f32> = Vec::with_capacity(wave_table_size);
+
+    // Print String by words & Fill wave_table by letters
     for word in line.split_whitespace()
     {
         let morse_code: String = encode::encode(word).unwrap();
         println!("{}", morse_code);
 
         // Slice by letter and create audio data based on '.' or '-' to play in the end
+        for letter in morse_code.chars()
+        {
+            println!("Input: {}", letter);
 
+            if letter == '.' 
+            {
+                println!("dot");
+            }
+            else
+            {
+                println!("line");
+            }
+        }
     }
-
-    //oscillator::run();
-
-    let wave_table_size = 64;
-    let mut wave_table: Vec<f32> = Vec::with_capacity(wave_table_size);
 
     for n in 0..wave_table_size {
         wave_table.push((2.0 * std::f32::consts::PI * n as f32 / wave_table_size as f32).sin());
@@ -42,10 +53,5 @@ fn main()
 
     std::thread::sleep(std::time::Duration::from_secs(1));
 
-    //let (_stream, stream_handle) = OutputStream::try_default().unwrap();
-    //let mut sine = SineWave::new(880.0);  
-    //let _result = stream_handle.play_raw(sine.convert_samples());
-
-    //std::thread::sleep(std::time::Duration::from_secs(1));
 }
 
